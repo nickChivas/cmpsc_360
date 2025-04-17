118f43201b4ce0cf51158771cefce83ddbe0911d
Nicholas Carabio <nicholas.carabio@gmail.com>
2025-04-16 23:44:51 -0400

    Fixed bug

--- matrixMultiplication/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.psu.matrixmultiplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.psu.matrixmultiplication to javafx.fxml;
-    exports com.psu.matrixmultiplication;
-}
